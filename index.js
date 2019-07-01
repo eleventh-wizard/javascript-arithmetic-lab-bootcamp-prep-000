010c5dd7cce379fbbd71a23d4a3f1cd3c75f8a2d
Justin L Rackham <justin.rackham@gmail.com>
2019-07-01 07:23:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,14 +1,70 @@
-var a,b
+var a,b;
 
-a=1
-b=2
+a=1;
+b=2;
+n=10;
 
 function add(a,b) {
   
  return a+b;
 }
 
+function subtract(a,b) {
+  
+  return b-a;
+}
 
+function multiply(a,b) {
+  
+  return a*b;
+  
+}
 
+function divide(a,b) {
+  
+ return a/b;
+ 
+}
 
+function inc(n) {
+  
+  return n++;
+}
+
+function dec(n) {
+  
+  return n--;
+  
+}
+
+function makeInt(n) {
+  
+  return parseInt('n', 10);
+  
+}
+
+function makeInt(n) {
+  
+  n=words;
+  
+  return parseInt('n', 10);
+  
+}
+
+function preserveDecimal(n) {
+  
+  n=3.16;
+  
+  return parseFloat('n');
+  
+  
+}
+
+function preserveDecimal(n) {
+  
+  n=words;
+  
+  return parseFloat('n');
+  
+}
 
